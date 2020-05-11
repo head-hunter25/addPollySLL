c26a4f215972b378b780ef6b793ff4d55ed23a92
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2020-05-11 08:55:19 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Node {
+	
+	protected int exp, coeff;
+	protected Node next;
+	Node(int coeff, int exp)
+	{
+		this.coeff=coeff;
+		this.exp=exp;
+		this.next=null;
+	}
+	public int getExp()
+	{
+		return this.exp;
+	}
+	public int getCoeff()
+	{
+		return this.coeff;
+	}
+}
